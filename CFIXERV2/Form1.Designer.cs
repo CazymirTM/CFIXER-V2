b90c90572e1294faac1490e1845ade1e5ef85570
CeZeY <91396680+CazymirTM@users.noreply.github.com>
2025-09-22 23:29:17 +0300

    Add files via upload

---
+++ CFIXERV2/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace CFIXERV2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.lblSelection = new System.Windows.Forms.Label();
+            this.btnApply = new System.Windows.Forms.Button();
+            this.btnExitApp = new System.Windows.Forms.Button();
+            this.btnApplyAll = new System.Windows.Forms.Button();
+            this.lblLog = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.progressBarFixes = new System.Windows.Forms.ProgressBar();
+            this.btnContact = new System.Windows.Forms.Button();
+            this.lblWarning = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblSelection
+            // 
+            this.lblSelection.AutoSize = true;
+            this.lblSelection.BackColor = System.Drawing.Color.Yellow;
+            this.lblSelection.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSelection.Location = new System.Drawing.Point(131, 133);
+            this.lblSelection.Name = "lblSelection";
+            this.lblSelection.Size = new System.Drawing.Size(277, 19);
+            this.lblSelection.TabIndex = 3;
+            this.lblSelection.Text = "Please select what fix do you want to apply:";
+            // 
+            // btnApply
+            // 
+            this.btnApply.Location = new System.Drawing.Point(12, 196);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(114, 42);
+            this.btnApply.TabIndex = 4;
+            this.btnApply.Text = "Apply";
+            this.btnApply.UseVisualStyleBackColor = false;
+            this.btnApply.Click += new System.EventHandler(this.BtnApply_Click);
+            // 
+            // btnExitApp
+            // 
+            this.btnExitApp.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btnExitApp.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExitApp.Location = new System.Drawing.Point(11, 499);
+            this.btnExitApp.Name = "btnExitApp";
+            this.btnExitApp.Size = new System.Drawing.Size(77, 37);
+            this.btnExitApp.TabIndex = 5;
+            this.btnExitApp.Text = "Exit App";
+            this.btnExitApp.UseVisualStyleBackColor = false;
+            this.btnExitApp.Click += new System.EventHandler(this.BtnExitApp_Click);
+            // 
+            // btnApplyAll
+            // 
+            this.btnApplyAll.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApplyAll.Location = new System.Drawing.Point(11, 133);
+            this.btnApplyAll.Name = "btnApplyAll";
+            this.btnApplyAll.Size = new System.Drawing.Size(114, 57);
+            this.btnApplyAll.TabIndex = 7;
+            this.btnApplyAll.Text = "Apply all fixes";
+            this.btnApplyAll.UseVisualStyleBackColor = false;
+            this.btnApplyAll.Click += new System.EventHandler(this.BtnApplyAll_Click);
+            // 
+            // lblLog
+            // 
+            this.lblLog.AutoSize = true;
+            this.lblLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLog.Location = new System.Drawing.Point(270, 247);
+            this.lblLog.Name = "lblLog";
+            this.lblLog.Size = new System.Drawing.Size(60, 24);
+            this.lblLog.TabIndex = 8;
+            this.lblLog.Text = "Status";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "SFC Scan",
+            "DISM Tools",
+            "Network Fixes"});
+            this.checkedListBox1.Location = new System.Drawing.Point(131, 155);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(148, 49);
+            this.checkedListBox1.TabIndex = 9;
+            // 
+            // progressBarFixes
+            // 
+            this.progressBarFixes.Location = new System.Drawing.Point(131, 210);
+            this.progressBarFixes.Name = "progressBarFixes";
+            this.progressBarFixes.Size = new System.Drawing.Size(451, 34);
+            this.progressBarFixes.TabIndex = 10;
+            // 
+            // btnContact
+            // 
+            this.btnContact.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btnContact.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnContact.Location = new System.Drawing.Point(11, 244);
+            this.btnContact.Name = "btnContact";
+            this.btnContact.Size = new System.Drawing.Size(102, 23);
+            this.btnContact.TabIndex = 11;
+            this.btnContact.Text = "Contact Me";
+            this.btnContact.UseVisualStyleBackColor = false;
+            this.btnContact.Click += new System.EventHandler(this.BtnContact_Click);
+            // 
+            // lblWarning
+            // 
+            this.lblWarning.AutoSize = true;
+            this.lblWarning.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWarning.ForeColor = System.Drawing.Color.Red;
+            this.lblWarning.Location = new System.Drawing.Point(12, 549);
+            this.lblWarning.Name = "lblWarning";
+            this.lblWarning.Size = new System.Drawing.Size(329, 20);
+            this.lblWarning.TabIndex = 12;
+            this.lblWarning.Text = "WARNING FOR AUTHORIZED USERS:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 569);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(658, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Please use chkdsk /r in another terminal if you encounter dism issues, these are " +
+    "problematic issues that needs solving using that command";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CFIXERV2.Properties.Resources.logocc;
+            this.pictureBox1.Location = new System.Drawing.Point(132, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(772, 127);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::CFIXERV2.Properties.Resources.background;
+            this.ClientSize = new System.Drawing.Size(1041, 595);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblWarning);
+            this.Controls.Add(this.btnContact);
+            this.Controls.Add(this.progressBarFixes);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.lblLog);
+            this.Controls.Add(this.btnApplyAll);
+            this.Controls.Add(this.btnExitApp);
+            this.Controls.Add(this.btnApply);
+            this.Controls.Add(this.lblSelection);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "CFIXER V2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblSelection;
+        private System.Windows.Forms.Button btnApply;
+        private System.Windows.Forms.Button btnExitApp;
+        private System.Windows.Forms.Button btnApplyAll;
+        private System.Windows.Forms.Label lblLog;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.ProgressBar progressBarFixes;
+        private System.Windows.Forms.Button btnContact;
+        private System.Windows.Forms.Label lblWarning;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ CFIXERV2/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CFIXERV2
+{
+    public partial class MainForm : Form
+    {
+        private Dictionary<string, Func<Task>> fixes;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeFixes();
+
+            // Admin check
+            if (!IsRunningAsAdmin())
+            {
+                MessageBox.Show(
+                    "Please run this application as Administrator to allow fixes to work properly.",
+                    "Admin Required",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+            }
+
+            progressBarFixes.Minimum = 0;
+            progressBarFixes.Maximum = 100;
+            progressBarFixes.Value = 0;
+        }
+
+        private void InitializeFixes()
+        {
+            fixes = new Dictionary<string, Func<Task>>
+            {
+                { "SFC Scan", RunSFCAsync },
+                { "DISM Checks", RunDISMAsync },
+                { "Network Fixes", RunNetworkFixesAsync }
+            };
+
+            checkedListBox1.Items.Clear();
+            foreach (var fixName in fixes.Keys)
+            {
+                checkedListBox1.Items.Add(fixName);
+            }
+
+            lblLog.Text = "Select a fix and click Apply.";
+        }
+
+        private bool IsRunningAsAdmin()
+        {
+            var identity = System.Security.Principal.WindowsIdentity.GetCurrent();
+            var principal = new System.Security.Principal.WindowsPrincipal(identity);
+            return principal.IsInRole(System.Security.Principal.WindowsBuiltInRole.Administrator);
+        }
+
+        // ------------------------------
+        // SFC Scan - instruct user to run manually
+        // ------------------------------
+        private Task RunSFCAsync()
+        {
+            MessageBox.Show(
+                "SFC Scan must be run manually in a separate elevated Command Prompt.\n\n" +
+                "Open CMD as Administrator and run:\n\nsfc /scannow\n\n" +
+                "Then wait until it finishes.",
+                "SFC Scan Required",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+
+            return Task.CompletedTask;
+        }
+
+        // ------------------------------
+        // DISM - runs in separate elevated CMD window
+        // ------------------------------
+        private Task RunDISMAsync()
+        {
+            try
+            {
+                MessageBox.Show(
+                    "DISM will run in a separate elevated Command Prompt window.\n" +
+                    "Please wait until it completes.",
+                    "DISM Checks",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/k DISM /Online /Cleanup-Image /RestoreHealth",
+                    UseShellExecute = true,  // elevation
+                    Verb = "runas",
+                    CreateNoWindow = false
+                });
+            }
+            catch (Exception ex)
+            {
+                lblLog.Invoke((Action)(() => lblLog.Text = $"Failed to run DISM: {ex.Message}"));
+            }
+
+            return Task.CompletedTask;
+        }
+
+        // ------------------------------
+        // Network Fixes - fully automated
+        // ------------------------------
+        private async Task RunNetworkFixesAsync()
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string logFile = Path.Combine(desktopPath, "NetworkFixes_Log.txt");
+
+            var commands = new string[]
+            {
+                "ipconfig /flushdns",
+                "netsh winsock reset",
+                "netsh int ip reset"
+            };
+
+            foreach (var cmd in commands)
+            {
+                await Task.Run(() =>
+                {
+                    try
+                    {
+                        var process = new Process
+                        {
+                            StartInfo = new ProcessStartInfo
+                            {
+                                FileName = "cmd.exe",
+                                Arguments = "/c " + cmd,
+                                RedirectStandardOutput = true,
+                                UseShellExecute = false,
+                                CreateNoWindow = true
+                            }
+                        };
+
+                        process.Start();
+                        string output = process.StandardOutput.ReadToEnd();
+                        process.WaitForExit();
+
+                        File.AppendAllText(logFile, $"{DateTime.Now}: {cmd}\n{output}\n\n");
+                    }
+                    catch (Exception ex)
+                    {
+                        lblLog.Invoke((Action)(() => lblLog.Text = $"Error running '{cmd}': {ex.Message}"));
+                    }
+                });
+            }
+        }
+
+        // ------------------------------
+        // Apply single fix
+        // ------------------------------
+        private async void BtnApply_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a fix to apply.");
+                return;
+            }
+
+            string fixName = checkedListBox1.SelectedItem.ToString();
+            if (fixes.ContainsKey(fixName))
+            {
+                lblLog.Invoke((Action)(() =>
+                    lblLog.Text = fixName == "SFC Scan" || fixName == "DISM Checks"
+                        ? $"{fixName} will run in a separate window."
+                        : $"Applying {fixName}..."));
+
+                progressBarFixes.Value = 0;
+
+                await fixes[fixName]();
+
+                progressBarFixes.Value = 100;
+                lblLog.Invoke((Action)(() =>
+                    lblLog.Text = fixName == "SFC Scan" || fixName == "DISM Checks"
+                        ? $"{fixName} launched. Follow instructions in the window."
+                        : $"{fixName} applied successfully!"));
+            }
+        }
+
+        // ------------------------------
+        // Apply all checked fixes
+        // ------------------------------
+        private async void BtnApplyAll_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox1.CheckedItems.Count == 0)
+            {
+                MessageBox.Show("Please check at least one fix to apply.");
+                return;
+            }
+
+            int total = checkedListBox1.CheckedItems.Count;
+            int completed = 0;
+            progressBarFixes.Value = 0;
+
+            foreach (var item in checkedListBox1.CheckedItems)
+            {
+                string fixName = item.ToString();
+                if (fixes.ContainsKey(fixName))
+                {
+                    lblLog.Invoke((Action)(() =>
+                        lblLog.Text = fixName == "SFC Scan" || fixName == "DISM Checks"
+                            ? $"{fixName} will run in a separate window."
+                            : $"Applying {fixName}..."));
+
+                    await fixes[fixName]();
+
+                    completed++;
+                    int percent = (int)((completed / (float)total) * 100);
+                    progressBarFixes.Invoke((Action)(() => progressBarFixes.Value = percent));
+                }
+            }
+
+            lblLog.Invoke((Action)(() => lblLog.Text = "All selected fixes applied!"));
+        }
+
+        private void BtnExitApp_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnContact_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Replace this with your actual Discord profile link
+                string discordUrl = "https://discord.com/users/493745450776526849";
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = discordUrl,
+                    UseShellExecute = true // ensures it opens in the default browser
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to open Discord link: {ex.Message}");
+            }
+        }
+    }
+}
